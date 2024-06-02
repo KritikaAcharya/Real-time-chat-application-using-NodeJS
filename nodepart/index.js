8609565097811d1e64fc7b2a399267ab2d32da9f
Kritika Acharya <acharyakritika44@gmail.com>
2024-06-02 18:52:52 +0545

    dealing with error

--- nodepart/index.js
+++ nodepart/index.js
@@ -5,8 +5,9 @@ const users={};
 
 io.on('connection', socket =>{
     socket.on('new-user-joined', name =>{
+        console.log("new user", name)
         users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name)
+        socket.broadcast.emit('user-joined',name);
     });
 
     socket.on('send', message =>{
