6dc95aca8030332898a741f01c7004358a3b3a1f
Kritika Acharya <acharyakritika44@gmail.com>
2024-06-02 19:00:57 +0545

    starting again

--- js/client.js
+++
@@ -1,9 +0,0 @@
-const socket = io('http://localhost:8000');
-
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector(".container");
-
-
-const name = prompt("enter your chatname");
-socket.emit('new-user-joined',name)
