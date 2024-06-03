c31733407d209e3b251e5b71d6b34246422d3fef
Kritika Acharya <acharyakritika44@gmail.com>
2024-06-03 09:42:12 +0545

    working with frontend

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require("http");
 const express = require("express");
+const exp = require("constants");
 
 const app = express();
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
 
 server.listen(port,()=>{
     console.log("server started at "+port )
