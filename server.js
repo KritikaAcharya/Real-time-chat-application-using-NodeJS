8b90c2365340961301ca1b134645686b340df863
Kritika Acharya <acharyakritika44@gmail.com>
2024-06-04 07:45:45 +0545

    connected

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/public'));
 
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 })
 
 server.listen(port,()=>{
-    console.log("server started at "+port )
+    console.log("server started at "+port );
 })
