b0e398168277e36683f2aaa8113e4366bcc47885
Kritika Acharya <acharyakritika44@gmail.com>
2024-06-07 10:29:02 +0545

    completed

--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,10 @@ const socket = io();
 
 var username;
 var chats=document.querySelector(".chats")
+var users_list=document.querySelector(".users-list")
+var users_count=document.querySelector(".users-count")
+var msg_send=document.querySelector("#user-send")
+var user_msg=document.querySelector("#user-msg")
 
 
 username=prompt("enter your username: ");
@@ -19,8 +23,50 @@ function userJoinLeft(name,status){
     let content=`<p><b>${name}</b> ${status} the chat </p>`;
     div.innerHTML=content;
     chats.appendChild(div);
+    chats.scrollTop=chats.scrollHeight;
+
 }
 
 socket.on('user-disconnected',(user)=>{
     userJoinLeft(user,'left')
+})
+
+socket.on('user-list',(user)=>{
+    users_list.innerHTML="";
+    users_arr=Object.values(user);
+    for(i=0;i<users_arr.length; i++){
+        let p=document.createElement('p');
+        p.innerText=users_arr[i];
+        users_list.appendChild(p);
+    }
+    users_count.innerHTML=users_arr.length;
+})
+
+msg_send.addEventListener('click',()=>{
+    let data ={
+        user:username,
+        msg:user_msg.value
+    };
+    if(user_msg.value!=''){
+        appendMessage(data,'outgoing');
+        socket.emit('message',data);
+        user_msg.value='';
+    }
+});
+
+function appendMessage(data,status){
+    let div=document.createElement('div');
+    div.classList.add('message',status);
+    let content=`
+    <h5>${data.user}</h5>
+    <p>${data.msg}</p>
+    `;
+    div.innerHTML=content;
+    chats.appendChild(div);
+    chats.scrollTop=chats.scrollHeight;
+}
+
+socket.on('message',(data)=>{
+    appendMessage(data,'incoming');
+
 })
